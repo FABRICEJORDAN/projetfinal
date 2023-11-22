d1df749a795bfc5bf8ad1664ad673fb9b8f8ea8d
FABRICEJORDAN <fabrice-laura.tatue-kegom.edu@groupe-gema.com>
2023-11-22 17:03:20 +0100

    energis

--- pages/type d'enegie ⚡.py
+++ pages/type d'enegie ⚡.py
@@ -4,6 +4,7 @@ import requests
 import pandas as pd
 import plotly.express as px
 from mod_data import pollution
+from plotly import graph_objs as go
 
 st.set_page_config(
     page_title="SCEBEC",
@@ -12,10 +13,74 @@ st.set_page_config(
 
 po=pollution()
 
-st.markdown(f'<h1 style="color:black;font-size:24px; text-align: center;text-decoration: underline">{"Consommation annauel des batiments en fonction du type denergie"}</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="color:black;font-size:24px; text-align: center;text-decoration: underline">{"Consommation annuel de chaque energie(en Kbtu) par batiment"}</h1>', unsafe_allow_html=True)
 
-fig = px.line(po,
-              x="SteamUse(kBtu)", y="PropertyName", 
-              markers=True,
-             )
-st.plotly_chart(fig)
+# city_counts = po.groupby('BuildingType').size().reset_index(name='Counts')
+
+# Affichage du nombre de lignes par ville
+#st.write('Nombre dhabitations par type:')
+#st.write(city_counts)
+choix = st.radio(
+        "Voir la consommation energetique 👇 ",
+        ["Sur tout le dataframe", "Par type dhabitation","generalité"],
+        key="visibility",
+        horizontal=True,
+    )
+if choix == 'Par type dhabitation':
+    valeurs_uniques = po['BuildingType'].unique()
+
+    selected_option = st.selectbox('Selectionner un type dhabitation pour voir les les batiments les plus energivores', valeurs_uniques)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.write('Je souhaite voir les')
+    with col2:
+        n = st.number_input("", value=6, step=1, format="%d")
+    with col3:
+        st.write("Habitations les plus énergivores")
+
+    filtre = po[po['BuildingType'] == selected_option]
+    first=filtre.sort_values(by='Electricity(kBtu)', ascending=False)[:n]
+
+    data2 = [
+        go.Scatter(x = first['PropertyName'], y=first["Electricity(kBtu)"], name='Quantité électricité utilisé', text=first["Electricity(kBtu)"]),
+        go.Scatter(x = first['PropertyName'], y = first["NaturalGas(kBtu)"], name='Quantité Gas utilisé', text=first["NaturalGas(kBtu)"]),
+        go.Scatter(x = first['PropertyName'], y = first["SteamUse(kBtu)"], name='Quantité vapeur utilisé', text=first["SteamUse(kBtu)"])
+    ]
+    fig2 = go.Figure(data=data2)
+
+    st.plotly_chart(fig2)
+
+    st.write(first.loc[:,['PropertyName','Electricity(kBtu)','NaturalGas(kBtu)','SteamUse(kBtu)']])
+elif choix == 'generalité':
+    st.write('En général voici la quantité denergie consommé par type dhabitation')
+    tp= po.groupby("BuildingType", as_index=False).agg({'Electricity(kBtu)': 'sum', 'NaturalGas(kBtu)': 'sum','SteamUse(kBtu)': 'sum'}).reset_index()
+
+    data = [
+        go.Scatter(x = tp['BuildingType'], y=tp["Electricity(kBtu)"], name='Quantité électricité utilisé', text=tp["Electricity(kBtu)"]),
+        go.Scatter(x = tp['BuildingType'], y = tp["NaturalGas(kBtu)"], name='Quantité Gas utilisé', text=tp["NaturalGas(kBtu)"]),
+        go.Scatter(x = tp['BuildingType'], y = tp["SteamUse(kBtu)"], name='Quantité vapeur utilisé', text=tp["SteamUse(kBtu)"])
+    ]
+    fig = go.Figure(data=data)
+    st.plotly_chart(fig)
+
+else:
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.write('Je souhaite voir les')
+    with col2:
+        m = st.number_input("", value=6, step=1, format="%d")
+    with col3:
+        st.write("Habitations les plus énergivores")
+
+    first=po.sort_values(by='Electricity(kBtu)', ascending=False)[:m]
+    data2 = [
+        go.Scatter(x = first['PropertyName'], y=first["Electricity(kBtu)"], name='Quantité électricité utilisé', text=first["Electricity(kBtu)"]),
+        go.Scatter(x = first['PropertyName'], y = first["NaturalGas(kBtu)"], name='Quantité Gas utilisé', text=first["NaturalGas(kBtu)"]),
+        go.Scatter(x = first['PropertyName'], y = first["SteamUse(kBtu)"], name='Quantité vapeur utilisé', text=first["SteamUse(kBtu)"])
+    ]
+    fig2 = go.Figure(data=data2)
+
+    st.plotly_chart(fig2)
+
+    st.write(first.loc[:,['PropertyName','Electricity(kBtu)','NaturalGas(kBtu)','SteamUse(kBtu)']])
